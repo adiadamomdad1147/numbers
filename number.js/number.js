2505345bfdbd1443d13aa1d7e38c891b3ecca031
adiadamomdad1147 <130950599+adiadamomdad1147@users.noreply.github.com>
2023-11-16 02:53:05 +0000

    number

---
+++ number.js/number.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.89674
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Math.random());
